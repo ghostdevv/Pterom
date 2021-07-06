ac2263a31c38ff43df18a3554d38fa9ddd53410a
cainthebest <75994858+cainthebest@users.noreply.github.com>
2021-07-06 22:32:35 +0100

    re brand

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-export default class Typeactyl {
+export default class Pterom {
     packMode: 'CLIENT' | 'APP';
     hostUrl: string;
     apiKey: string;
 
-    constructor(mode: 'CLIENT' | 'APP',host: string, key: string) {
+    constructor(mode: 'CLIENT' | 'APP', host: string, key: string) {
         this.hostUrl = host;
         this.apiKey = key;
         this.packMode = mode;
